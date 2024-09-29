f9f9124902379acef4462ac11e1f4efa1ec2eea5
Samw662 <amadorsamir307@gmail.com>
2024-09-29 17:47:02 +0000

    bangkk: AnyKernel3

---
+++ anykernel.sh
@@ -0,0 +1,36 @@
+# AnyKernel3 Ramdisk Mod Script
+# osm0sis @ xda-developers
+
+properties() { '
+kernel.string=—WearyStars-R1+
+do.devicecheck=1
+do.modules=1
+do.systemless=0
+do.cleanup=1
+do.cleanuponabort=0
+device.name1=bangkk
+supported.versions=14.0
+supported.patchlevels=
+'; }
+
+block=/dev/block/bootdevice/by-name/boot;
+is_slot_device=1;
+ramdisk_compression=auto;
+patch_vbmeta_flag=auto;
+
+. tools/ak3-core.sh;
+
+set_perm_recursive 0 0 755 644 $ramdisk/*;
+set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
+
+dump_boot;
+write_boot;
+
+block=vendor_boot;
+is_slot_device=1;
+ramdisk_compression=auto;
+patch_vbmeta_flag=auto;
+
+reset_ak;
+split_boot;
+flash_boot;
